4c422fd62363060027033d4a949b5190d9e5e638
ChengHuang-CH <huangcheng1994@126.com>
2022-05-02 19:08:28 +0100

    carla-uam-daaset01

--- mmdet3d/datasets/carla_uam_dataset.py
+++ mmdet3d/datasets/carla_uam_dataset.py
@@ -14,5 +14,91 @@ from .pipelines import Compose
 
 @DATASETS.register_module()
 class CarlaUamDataset(Custom3DDataset):
-    def __init__(self):
-        super().__init__()
+
+    CLASSES = ('Vehicle', 'Walker', 'Drone')
+
+    def __init__(self,
+                 data_root,
+                 ann_file,
+                 split,
+                 pipeline=None,
+                 classes=None,
+                 modality=None,
+                 box_type_3d='LiDAR',
+                 filter_empty_gt=True,
+                 test_mode=False
+                 ):
+        super().__init__(data_root=data_root,
+                         ann_file=ann_file,
+                         pipeline=pipeline,
+                         classes=classes,
+                         modality=modality,
+                         box_type_3d=box_type_3d,
+                         filter_empty_gt=filter_empty_gt,
+                         test_mode=test_mode)
+
+        if self.modality is None:
+            self.modality = dict(
+                use_camera=False,
+                use_lidar=True
+            )
+
+    def load_annotations(self, ann_file):
+        """Load annotations from ann_file.
+
+        Args:
+            ann_file (str): Path of the annotation file.
+
+        Returns:
+            list[dict]: List of annotations.
+        """
+
+    def get_data_info(self, index):
+        """Get data info according to the given index.
+
+        Args:
+            index (int): Index of the sample data to get.
+
+        Returns:
+            dict: Data information that will be passed to the data
+                preprocessing pipelines. It includes the following keys:
+
+                - sample_idx (str): Sample index.
+                - pts_filename (str): Filename of point clouds.
+                - file_name (str): Filename of point clouds.
+                - ann_info (dict): Annotation info.
+        """
+
+    def get_ann_info(self, index):
+        """Get annotation info according to the given index.
+
+        Args:
+            index (int): Index of the annotation data to get.
+
+        Returns:
+            dict: Annotation information consists of the following keys:
+
+                - gt_bboxes_3d (:obj:`LiDARInstance3DBoxes`):
+                    3D ground truth bboxes
+                - gt_labels_3d (np.ndarray): Labels of ground truths.
+                - gt_names (list[str]): Class names of ground truths.
+        """
+
+    def _build_default_pipeline(self):
+        """Build the default pipeline for this dataset."""
+        pipeline = [
+            dict(
+                type='LoadPointsFromFile',
+                coord_type='LIDAR',
+                load_dim=4,
+                use_dim=4,
+                file_client_args=dict(backend='disk')),
+            dict(
+                type='DefaultFormatBundle3D',
+                class_names=self.CLASSES,
+                with_label=False),
+            dict(type='Collect3D', keys=['points'])
+        ]
+        if self.modality['use_camera']:
+            pipeline.insert(0, dict(type='LoadImageFromFile'))
+        return Compose(pipeline)
